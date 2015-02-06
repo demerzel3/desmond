7e7406c3d7008a85dcbc99924f503314031702d6
Gabriele Genta <gabriele.genta@gmail.com>
2015-02-06 22:25:27 +0100

    Added fullscreen file drop

--- client/app/component.js
+++ client/app/component.js
@@ -121,7 +121,6 @@
         img.on('load', function() {
           var color = colorThief.getPalette(img[0], 3)[0];
           var styleName = attrs.colorFromImage || 'color';
-          console.log(styleName);
           var style = {};
           style[styleName] = 'rgb('+color[0]+','+color[1]+','+color[2]+')';
           element.css(style);
@@ -129,5 +128,48 @@
       }
     };
   });
+
+  Component.directive('dragOverScreen', ['$timeout', function($timeout) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        var leaveTimeout = null;
+        var enterCount = 0;
+        var leaveCount = 0;
+        var dragElement = null;
+
+        document.addEventListener('dragover', function(event) {
+          // allow drop!
+          event.preventDefault();
+       	}, false);
+
+        document.addEventListener('dragenter', function(event) {
+          dragElement = event.target;
+          if (leaveTimeout) {
+            $timeout.cancel(leaveTimeout);
+            leaveTimeout = null;
+          }
+          element.addClass('dragover');
+          enterCount++;
+        }, false);
+
+        document.addEventListener('dragleave', function(event) {
+          if (event.target === dragElement) {
+            dragElement = null;
+            leaveTimeout = $timeout(function () {
+              element.removeClass('dragover');
+              leaveTimeout = null;
+            }, 500);
+            leaveCount++;
+          }
+       	}, false);
+
+        document.addEventListener('drop', function(event) {
+          element.removeClass('dragover');
+          event.preventDefault();
+        }, false);
+      }
+    }
+  }]);
 
 })(window.jQuery, window.angular);
