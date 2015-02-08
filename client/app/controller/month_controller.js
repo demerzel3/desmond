002075e9bb65a844eab4470cf3eea592d9e71d06
Gabriele Genta <gabriele.genta@gmail.com>
2015-02-08 19:11:15 +0100

    Added funnel graphics and row coloring to month view.

--- client/app/controller/month_controller.js
+++ client/app/controller/month_controller.js
@@ -1,7 +1,7 @@
-(function($, angular) {
+(function($, angular, Desmond) {
 
-  var MonthController = function($stateParams, $scope, MovementsRepository, Statistics) {
-    var ctrl = this;
+  var MonthController = function($stateParams, $scope, $timeout, MovementsRepository, Statistics) {
+    this.$timeout = $timeout;
     this.month = $stateParams.month;
     this.stat = Statistics.outgoingByCategoryByMonth;
 
@@ -10,10 +10,11 @@
     this.categories = [];
     this.total = 0;
 
-    if (ctrl.movements.all.length > 0) {
+    if (this.movements.all.length > 0) {
       this.initData();
     }
 
+    var ctrl = this;
     $scope.$watch('ctrl.movements.all', function(movements, oldValue) {
       if (movements === oldValue) {
         return;
@@ -22,10 +23,12 @@
       ctrl.initData();
     });
   };
-  MonthController.$inject = ['$stateParams', '$scope', 'MovementsRepository', 'Statistics'];
+  MonthController.$inject = ['$stateParams', '$scope', '$timeout', 'MovementsRepository', 'Statistics'];
 
   MonthController.prototype.initData = function() {
     var ctrl = this;
+
+    this.registerCategoriesStyles(this.stat.categories);
 
     var monthIndex = _.findIndex(this.stat.months, function(month) {
       return month.id === ctrl.month;
@@ -72,8 +75,112 @@
     this.total = _.reduce(this.categories, function(sum, category) {
       return sum + category.monthTotal;
     }, 0);
+
+    this.$timeout(function() {
+      ctrl.updateFunnel();
+    });
   };
 
-  angular.module('Desmond').controller('MonthController', MonthController);
+  MonthController.prototype.registerCategoriesStyles = function(categories) {
+    var sheet = (function() {
+    	var style = document.createElement("style");
+    	// WebKit hack :(
+    	style.appendChild(document.createTextNode(""));
+    	document.head.appendChild(style);
+    	return style.sheet;
+    })();
+    categories.forEach(function(category, index) {
+      var hsl = tinycolor(category.color).toHsl();
+      hsl.s = Math.max(hsl.s, hsl.s-0.4);
+      hsl.l = 0.95;
+      var color = tinycolor(hsl);
+      sheet.insertRule('table.table-movements tr[category="'+category._id+'"] {background-color: '+color.toString()+'}', index);
+    });
+    console.log(sheet);
+  };
 
-})(window.jQuery, window.angular);
+  MonthController.prototype.updateFunnel = function() {
+    var $el = $('#monthView');
+    var categories = $el.find('.categories > div');
+    var rows = $el.find('tbody > tr').toArray();
+    var movements = [].concat(this.filteredMovements);
+
+    var items = _.map(this.categories, function(category, index) {
+      var catEl = categories.eq(index);
+      var pos = catEl.position();
+      var funnelItem = {
+        color: category.color,
+        left: {
+          from: pos.top,
+          to: pos.top + catEl.height()
+        }
+      };
+
+      var catRows = [];
+      if (category._id) {
+        while (movements.length > 0 && movements[0].category && movements[0].category._id === category._id) {
+          catRows.push(rows.shift());
+          movements.shift();
+        }
+      } else {
+        while (movements.length > 0 && !movements[0].category) {
+          catRows.push(rows.shift());
+          movements.shift();
+        }
+      }
+
+      funnelItem.right = {
+        from: $(catRows[0]).position().top + 1
+      };
+      var rowsHeight = _.reduce(catRows, function(totalHeight, row) {
+        return totalHeight + $(row).height();
+      }, 0) - 1;
+      funnelItem.right.to = funnelItem.right.from + rowsHeight;
+
+      return funnelItem;
+    });
+
+    var funnelEl = $el.find('.funnel').empty();
+    var size = {width: funnelEl.width(), height: funnelEl.height()};
+
+    var paper = Raphael(funnelEl[0]);
+    var paths = _.map(items, function(item) {
+      var pathString = 'M'+0+','+item.left.from+'L'+size.width+','+item.right.from
+        +'L'+size.width+','+item.right.to+'L'+0+','+item.left.to+'Z';
+      var path = paper.path(pathString);
+      path.attr({
+        fill: item.color,
+        stroke: 'transparent'
+      });
+      return path;
+    });
+
+    var updateFunnel = function() {
+      var scrollTop = $el.find('.main-box')[0].scrollTop;
+      var newSize = {width: size.width, height: funnelEl.height()};
+      if (newSize.height !== size.height) {
+        paper.setSize(newSize.width, newSize.height);
+
+        // update from
+        items.forEach(function(item, index) {
+          var catEl = categories.eq(index);
+          var pos = catEl.position();
+          item.left.from = pos.top;
+          item.left.to = pos.top + catEl.height();
+        });
+      }
+      paths.forEach(function(path, index) {
+        var item = items[index];
+        var pathString = 'M'+0+','+item.left.from+'L'+newSize.width+','+(item.right.from-scrollTop)
+          +'L'+newSize.width+','+(item.right.to-scrollTop)+'L'+0+','+item.left.to+'Z';
+        path.attr('path', pathString);
+      });
+    };
+
+    $('.main-box').on('scroll', updateFunnel);
+    $(window).resize(updateFunnel);
+  };
+
+  Desmond.controller('MonthController', MonthController);
+
+})(window.jQuery, window.angular, window.angular.module('Desmond'));
