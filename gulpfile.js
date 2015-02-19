bc7fc02cd4dc9981228547a8af7ca8a05cabe82e
Gabriele Genta <gabriele.genta@gmail.com>
2015-02-20 00:54:37 +0100

    Add gulp task to invoke traceur for automatic compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,30 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
+var shell = require('gulp-shell');
+var rename = require('gulp-rename');
+var del = require('del');
 var nib = require('nib');
+
+/**
+ * gulp has no support for single file traceur output, I had to use the command directly.
+ * @see https://github.com/google/traceur-compiler/issues/1626
+ */
+gulp.task('traceur:compile', function () {
+  return gulp.src(['./client/app/routing.js'], {read: false})
+    .pipe(shell(['traceur --modules inline --module "<%= file.path %>" --out ./client/app/build.js']));
+});
+
+gulp.task('build:move', ['traceur:compile'], function() {
+  return gulp.src(['./client/app/build.js'])
+    .pipe(rename('app.js'))
+    .pipe(gulp.dest('./client/build'));
+});
+
+gulp.task('build:clean', ['build:move'], function() {
+  del('./client/app/build.js');
+});
+
+gulp.task('build', ['traceur:compile', 'build:move', 'build:clean']);
 
 gulp.task('style:compile', function () {
   gulp.src('./style/main.styl')
@@ -11,6 +35,7 @@ gulp.task('style:compile', function () {
     .pipe(gulp.dest('./client/css'));
 });
 
-gulp.task('watch', function() {
+gulp.task('default', ['build', 'style:compile'], function() {
   gulp.watch(['./style/main.styl'], ['style:compile']);
+  gulp.watch(['./client/app/**/*.js'], ['build']);
 });
