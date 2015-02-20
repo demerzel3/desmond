931d850403de7dd0b3bb856c989d99b266117f64
Gabriele Genta <gabriele.genta@gmail.com>
2015-02-20 23:11:28 +0100

    Upgrade ImportModalController to ES6 syntax

--- src/client/controller/import_modal_controller.js
+++ src/client/controller/import_modal_controller.js
@@ -1,24 +1,24 @@
-  var ImportModalController = function($timeout, $modalInstance, document) {
+class ImportModalController {
+  constructor($timeout, $modalInstance, document) {
     _.each(document.movements, function(movement, index) {
       movement._id = index;
     });
 
-    //console.log(document);
-
     this.$modalInstance = $modalInstance;
     this.movements = document.movements;
-    var ctrl = this;
-    $timeout(function() {
-      ctrl.selectedItems = [].concat(document.movements);
+    $timeout(() => {
+      this.selectedItems = [].concat(document.movements);
     });
-  };
-  ImportModalController.$inject = ['$timeout', '$modalInstance', 'document'];
+  }
 
-  ImportModalController.prototype.ok = function() {
+  ok() {
     _.each(this.selectedItems, function(movement) {
       delete movement._id;
     });
     this.$modalInstance.close(this.selectedItems);
-  };
+  }
+}
 
-  export default ImportModalController;
+ImportModalController.$inject = ['$timeout', '$modalInstance', 'document'];
+
+export default ImportModalController;
