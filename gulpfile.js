ab0a12ce872f36116da66848e106b42fd55e5b1c
Gabriele Genta <gabriele.genta@gmail.com>
2015-02-21 18:49:23 +0100

    Switched compilation to using gulp-traceur
    Pro: availability of intermediate build files for inspection of compilation issues under /build + unminified concatenated version of the source in client/app
    Con: had to add almond.js as a dependency, a lightweight runtime for AMD modules in the browser (~15K unminified)
    As before: complete sourcemap support

--- gulpfile.js
+++ gulpfile.js
@@ -6,40 +6,62 @@ var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
 var nib = require('nib');
-var path = require('path');
-var vinylPaths = require('vinyl-paths');
+var traceur = require('gulp-traceur');
+var concat = require('gulp-concat');
+var insert = require('gulp-insert');
+var through = require('through2');
 
 var paths = {
-  clientSrcRoot: path.resolve('./src/client/'),
-  mainFile: './src/client/routing.js',
-  buildFile: './src/client/desmond.js',
-  buildFileMap: './src/client/desmond.map'
+  source: {
+    dir: './src/client',
+    files: './src/client/**/*.js',
+    viewsFiles: './src/client/views/**/*.html'
+  },
+  build: {
+    mainModule: 'routing',
+    dir: './build/client',
+    files: './build/client/**/*.js'
+  },
+  dist: {
+    dir: './client/js',
+    file: './client/js/desmond.js',
+    filename: 'desmond.js'
+  }
 };
 
-/**
- * gulp has no support for single file traceur output, I had to use the command directly.
- * @see https://github.com/google/traceur-compiler/issues/1626
- */
-gulp.task('traceur:compile', function () {
-  return gulp.src(paths.mainFile, {read: false})
-    .pipe(shell(['traceur --modules inline --source-maps file --source-root "'+paths.clientSrcRoot+'" --module "<%= file.path %>" --out '+paths.buildFile]));
-});
-
-gulp.task('uglify', ['traceur:compile'], function() {
-  gulp.src(paths.buildFile)
-    .pipe(rename('desmond.min.js'))
+gulp.task('js:compile', function() {
+  return gulp.src(paths.source.files/*, {base: paths.source.dir}*/)
+    // convert every ES6 source file to an AMD module
+    .pipe(traceur({sourceMaps: 'inline', modules: 'amd'}))
     .pipe(sourcemaps.init({loadMaps: true}))
-      .pipe(uglify())
-    .pipe(sourcemaps.write({sourceRoot: '/src'}))
-    .pipe(gulp.dest('./client/app'));
+    // name modules after their filename
+    .pipe(through.obj(function(file, enc, callback) {
+      var moduleName = file.relative.match(/^([a-z_\/]+)\.js$/)[1];
+      file.contents = new Buffer(String(file.contents).replace('define([', 'define("' + moduleName + '", ['));
+      callback(null, file);
+    }))
+    // output intermediate build files (for debug purposes)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.build.dir));
 });
 
-gulp.task('build:clean', ['traceur:compile', 'uglify'], function() {
-  gulp.src([paths.buildFile, paths.buildFileMap])
-    .pipe(vinylPaths(del));
+gulp.task('js:concat', ['js:compile'], function() {
+  gulp.src(paths.build.files)
+    .pipe(sourcemaps.init({loadMaps: true, debug: true}))
+    .pipe(concat(paths.dist.filename))
+    .pipe(insert.append('require(\'' + paths.build.mainModule + '\');'))
+    .pipe(sourcemaps.write('.', {debug: true}))
+    .pipe(gulp.dest(paths.dist.dir));
 });
 
-gulp.task('build', ['traceur:compile', 'uglify', 'build:clean']);
+gulp.task('js:uglify', ['js:concat'], function() {
+  gulp.src(paths.dist.file)
+    .pipe(rename('desmond.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dist.dir));
+});
+
+gulp.task('js:build', ['js:compile', 'js:concat', 'js:uglify']);
 
 gulp.task('style:compile', function () {
   gulp.src('./style/main.styl')
@@ -50,7 +72,7 @@ gulp.task('style:compile', function () {
     .pipe(gulp.dest('./client/css'));
 });
 
-gulp.task('default', ['build', 'style:compile'], function() {
+gulp.task('default', ['js:build', 'style:compile'], function() {
   gulp.watch(['./style/main.styl'], ['style:compile']);
-  gulp.watch(['./src/client/**/*.js', '!'+paths.buildFile], ['build']);
+  gulp.watch(['./src/client/**/*.js'], ['js:build']);
 });
