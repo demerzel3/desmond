f47d9e51fa7652dbcef1de63164e986875ed7d77
Gabriele Genta <gabriele.genta@gmail.com>
2015-02-23 22:34:03 +0100

    Update service.js to ES6 syntax

--- src/client/service.js
+++ src/client/service.js
@@ -6,67 +6,81 @@ import Statistics from 'service/statistics.js';
  * @param [priority]
  * @constructor
  */
-var Rule = function(name, callable, priority) {
-  this.name = name;
-  this.callable = callable;
-  this.priority = priority || 0;
-};
+class Rule {
+  constructor(name, callable, priority = 0) {
+    this.name = name;
+    this.callable = callable;
+    this.priority = priority;
+  }
+}
 
-var RulesContainer = function() {
-  this.rules = [];
-};
-RulesContainer.prototype.rule = function(name, callable, priority) {
-  var rule = new Rule(name, callable, priority);
-  this.rules.push(rule);
-  this.rules = _.sortBy(this.rules, 'priority');
-};
-/**
- * Apply all the rules to a movement, stopping at the first that applies successfully.
- *
- * @param movement
- */
-RulesContainer.prototype.applyAll = function(movement) {
-  _.each(this.rules, function(rule) {
-    if (rule.callable.apply(movement, [movement])) {
-      movement._appliedRule = rule.name;
-      return false;
+
+
+class RulesContainer {
+  constructor() {
+    this.rules = [];
+  }
+
+  rule(name, callable, priority) {
+    var rule = new Rule(name, callable, priority);
+    this.rules.push(rule);
+    this.rules = _.sortBy(this.rules, 'priority');
+  }
+
+  /**
+   * Apply all the rules to a movement, stopping at the first that applies successfully.
+   *
+   * @param movement
+   */
+  applyAll(movement) {
+    for (let rule of this.rules) {
+      if (rule.callable.apply(movement, [movement])) {
+        movement._appliedRule = rule.name;
+        break;
+      }
     }
-  });
-};
+  }
+}
 
 
 
+class PageMetadata {
+  constructor() {
+    this.title = "Desmond";
+  }
+}
 
-var PageMetadata = function() {
-  this.title = "Desmond";
-};
 
 
+class FileHasher {
+  constructor($q) {
+    this.$q = $q;
+  }
 
-var FileHasher = function($q) {
-  this.$q = $q;
-};
+  /**
+   * Adds an hash filed to the file and returns it (promise).
+   *
+   * @param file
+   * @returns {Promise<File>}
+   */
+  hashFile(file) {
+    var readData = this.$q(function(resolve, reject) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        resolve(e.target.result);
+      };
+      reader.readAsBinaryString(file);
+    });
+
+    return readData.then(function(binaryData) {
+      file.hash = SparkMD5.hashBinary(binaryData);
+      return file;
+    });
+  }
+}
 FileHasher.$inject = ['$q'];
-/**
- * Adds an hash filed to the file and returns it (promise).
- *
- * @param file
- * @returns {Promise<File>}
- */
-FileHasher.prototype.hashFile = function(file) {
-  var readData = this.$q(function(resolve, reject) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      resolve(e.target.result);
-    };
-    reader.readAsBinaryString(file);
-  });
 
-  return readData.then(function(binaryData) {
-    file.hash = SparkMD5.hashBinary(binaryData);
-    return file;
-  });
-};
+
 
 var Service = angular.module('Desmond.Service', []);
 Service.service('RulesContainer', RulesContainer);
