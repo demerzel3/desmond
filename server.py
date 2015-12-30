0c4c71f306de1780e7e9b3f727fcc0f5ea685f43
Gabriele Genta <gabriele.genta@gmail.com>
2015-12-30 01:51:05 +0100

    Add standalone python server script

---
+++ server.py
@@ -0,0 +1,19 @@
+import BaseHTTPServer
+import os.path
+from mimetypes import MimeTypes
+from urlparse import urlparse
+
+mime = MimeTypes()
+
+class Handler( BaseHTTPServer.BaseHTTPRequestHandler ):
+    def do_GET( self ):
+        path = urlparse('client' + self.path).path
+        if not os.path.isfile(path):
+            path = 'client/index.html'
+        self.send_response(200)
+        self.send_header( 'Content-type', mime.guess_type(path)[0] )
+        self.end_headers()
+        self.wfile.write( open(path).read() )
+
+httpd = BaseHTTPServer.HTTPServer( ('127.0.0.1', 9999), Handler )
+httpd.serve_forever()
